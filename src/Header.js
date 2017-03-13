8f330dc192213df7ca2f23c2063bb37ccd288b8c
Jsumulong <sumulong28@gmail.com>
2017-03-13 13:00:56 -0500

    add style to Header component, create ListItem component

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import ListItem from './ListItem';
 
 class Header extends Component {
 	render() {
+		const outerDivStyle = {
+			"backgroundColor": "white",
+			"height": 50
+		}
+		const ulStyle = {
+			"listStyle": "none",
+			"padding": 0,
+			"display": "flex",
+			"justifyContent": "center",
+			"alignItems": "center"
+		}
 		return (
-			<div>
-				<ul>
-					<li>Home</li>
-					<li>About Us</li>
-					<li>Search</li>
-					<li>Help</li>
-					<li>Shopping Cart</li>
+			<div style={outerDivStyle}>
+				<ul style={ulStyle}>
+					<ListItem content="Home"/>
+					<ListItem content="About Us"/>
+					<ListItem content="Search"/>
+					<ListItem content="Help"/>
+					<ListItem content="Shopping Cart"/>
 				</ul>
 			</div>
 		)
