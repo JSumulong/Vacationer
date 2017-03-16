45e708fcdfbdbbd86a3a126fc755d9799e6d13d8
Jsumulong <sumulong28@gmail.com>
2017-03-16 14:03:20 -0500

    update Header component with stickyHeader function on ComponentDidMount lifecycle

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import ListItem from './ListItem';
 
 class Header extends Component {
+	stickyHeader() {
+		const header = document.querySelector('.header-component');
+		const headerPosition = header.offsetTop;
+		function attachHeader() {
+			if (window.scrollY >= headerPosition) {
+				document.body.classList.add('fixed-header');
+			} else {
+				document.body.classList.remove('fixed-header');
+			}
+		}
+		window.addEventListener('scroll', attachHeader);
+	}
+	componentDidMount() {
+		this.stickyHeader();
+	}
 	render() {
 		const outerDivStyle = {
 			"backgroundColor": "white",
@@ -18,7 +33,7 @@ class Header extends Component {
 			"alignItems": "center"
 		}
 		return (
-			<div style={outerDivStyle}>
+			<div className="header-component" style={outerDivStyle}>
 				<ul style={ulStyle}>
 					<ListItem content="Home"/>
 					<ListItem content="About Us"/>
